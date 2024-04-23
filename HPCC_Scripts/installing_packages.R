b06955f35eb8d9bda81400e55bee07037798a1ae
klankowi <klankowicz@gmri.org>
2024-04-23 14:57:41 -0400

    rm trash

--- HPCC_Scripts/installing_packages.R
+++
@@ -1,9 +0,0 @@
-## Installing necessary packages in R
-
-install.packages('devtools')
-install.packages('TMB')
-install.packages('units')
-install.packages('INLA')
-install.packages('here')
-install.packages('sf')
-install.packages('splines')
